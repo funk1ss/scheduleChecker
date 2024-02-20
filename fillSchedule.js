c6e20bd00b039923b75523812fe0486f9a4f42e9
funk1ss <vova.shumakov.76@mail.ru>
2024-02-20 14:30:16 +0600

    fixes

--- fillSchedule.js
+++ fillSchedule.js
@@ -5,19 +5,20 @@ const fillSchedule = () => {
     parentElement.classList.toggle("show");
     parentElement.innerHTML = "";
 
-
-    for (let day in schedule){
+    for (let day in schedule) {
         let itemTitle = document.createElement('div');
-        itemTitle.textContent = day
-        parentElement.appendChild(itemTitle)
-        let counter = 0
-        for (let item in schedule[day]){
-            counter++
+        itemTitle.textContent = day;
+        
+        parentElement.appendChild(itemTitle);
+        let counter = 0;
+        for (let item in schedule[day]) {
+            counter++;
+            let text = counter + ".  " + schedule[day][item];
+            let formattedText = text.replace(/\[(.*?)\]/g, '<span class="text-green-400">[$1]</span>');
             let listItem = document.createElement('div');
-            listItem.textContent = counter +".  "+ schedule[day][item]
-            itemTitle.appendChild(listItem)
-            itemTitle.classList.add("bg-zinc-600" ,"mb-2", "rounded", "p-3", "text-zinc-200")
-
+            listItem.innerHTML = formattedText;
+            itemTitle.appendChild(listItem);
+            itemTitle.classList.add("bg-zinc-600", "mb-2", "rounded", "p-3", "text-zinc-200");
         }
     }
 }
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const schedule = {
     "Понедельник": {
-        "one": "ИКТ",
-        "two": "Способы программирования",
-        "three": "Код программирования",
-        "four": "Физическая культура",
-        "five": "-"
+        "one": "[8:00] ИКТ [313каб.]",
+        "two": "[9:30] Способы программирования [310каб.]",
+        "three": "[11:00] Код программирования [211каб.]",
+        "four": "[12:50] Физическая культура",
+        "five": "[14:20] -"
     },
     "Вторник": {
-        "one": "-",
-        "two": "-",
-        "three": "Способы программирования",
-        "four": "Экономика",
-        "five": "Делопроизовдство"
+        "one": "[8:00] -",
+        "two": "[9:30] -",
+        "three": "[11:00] Способы программирования [310каб.]",
+        "four": "[12:50] Экономика [303каб.]",
+        "five": "[14:20] Делопроизовдство [104каб.]"
     },
     "Среда": {
-        "one": "Способы программирования",
-        "two": "Код программирования",
-        "three": "Культурология",
-        "four": "-",
-        "five": "-"
+        "one": "[8:00] Способы программирования [310каб.]",
+        "two": "[9:30] Код программирования [211каб.]",
+        "three": "[10:50] Культурология [106каб.]",
+        "four": "[12:50] -",
+        "five": "[14:20] -"
     },
     "Четверг": {
-        "one": "-",
-        "two": "ИКТ",
-        "three": "Экономика",
-        "four": "Код программирования",
-        "five": "Делопроизводство"
+        "one": "[8:00] -",
+        "two": "[9:30] ИКТ [313каб.]",
+        "three": "[11:00] Экономика [303каб.]",
+        "four": "[12:50] Код программирования [211каб.]",
+        "five": "[14:20] Делопроизводство [104каб.]"
     },
     "Пятница": {
-        "one": "Нейронные сети",
-        "two": "Основы социологии и политологии",
-        "three": "Физическая культура",
-        "four": "ИКТ",
-        "five": "-"
+        "one": "[8:00] Нейронные сети [308каб.]",
+        "two": "[9:30] Основы социологии и политологии [106каб.]",
+        "three": "[11:00] Физическая культура",
+        "four": "[12:50] ИКТ [313каб.]",
+        "five": "[14:20] -"
     }
 }
 
@@ -70,6 +70,7 @@ const currentDay = () => {
 const nextDayInfo = () => {
     let nextDay
     let arrivingTime
+    let endTime
     switch (currentDay()) {
         case "Понедельник":
             nextDay = "Вторник"
@@ -100,29 +101,45 @@ const nextDayInfo = () => {
     if (nextDay == "Выходной"){
         arrivingTime = "-"
     }
-    else if (schedule[nextDay].one != "-"){
+    else if (!schedule[nextDay].one.includes("-")){
         arrivingTime = "8:00"
     }
-    else if (schedule[nextDay].two != "-"){
+    else if (!schedule[nextDay].two.includes("-")){
         arrivingTime = "9:30"
     }
-    else if (schedule[nextDay].three != "-"){
+    else if (!schedule[nextDay].three.includes("-")){
         arrivingTime = "11:00"
     }
-    else if (schedule[nextDay].four != "-"){
+    else if (!schedule[nextDay].four.includes("-")){
         arrivingTime = "12:20"
+    }
+
+
+    if (nextDay == "Выходной"){
+        arrivingTime = "-"
+    }
+    else if (schedule[nextDay].four.includes("-")){
+        endTime = "12:20"
+    }
+    else if (schedule[nextDay].five.includes("-")){
+        endTime = "14:10"
     }
     
 
-    if(nextDay != "Выходной"){
+    if (nextDay != "Выходной") {
+        let formattedSubjects = subjectsList.map(subject => {
+            return subject.replace(/\[(.*?)\]/g, '<span class="text-green-500">[$1]</span>');
+        });
+    
         Swal.fire({
             title: nextDay,
-            html: '<div class="text-left">' + subjectsList.join("<br>") + '</div>',
+            html: '<div class="text-left">' + formattedSubjects.join("<br>") + '</div>',
             icon: 'info',
             footer: 'Пары начнутся в ' + arrivingTime,
             confirmButtonText: 'Ok',
-          })
+        });
     }
+    
     else{
         Swal.fire({
             title: nextDay,
@@ -147,27 +164,44 @@ const todaysInfo = () => {
     if (currentDay() == "Выходной"){
         arrivingTime = "-"
     }
-    else if (schedule[currentDay()].one != "-"){
+    else if (!schedule[currentDay()].one.includes("-")){
         arrivingTime = "8:00"
     }
-    else if (schedule[currentDay()].two != "-"){
+    else if (!schedule[currentDay()].two.includes("-")){
         arrivingTime = "9:30"
     }
-    else if (schedule[currentDay()].three != "-"){
+    else if (!schedule[currentDay()].three.includes("-")){
         arrivingTime = "11:00"
     }
-    else if (schedule[currentDay()].four != "-"){
+    else if (!schedule[currentDay()].four.includes("-")){
         arrivingTime = "12:20"
     }
 
-    if(currentDay() != "Выходной"){
+    if (currentDay() == "Выходной"){
+        arrivingTime = "-"
+    }
+    else if (schedule[currentDay()].four.includes("-")){
+        endTime = "12:20"
+    }
+    else if (schedule[currentDay()].five.includes("-")){
+        endTime = "14:10"
+    }
+    else{
+        endTime = "15:40"
+    }
+
+    if (currentDay() != "Выходной") {
+        let formattedSubjects = subjectsList.map(subject => {
+            return subject.replace(/\[(.*?)\]/g, '<span class="text-green-500">[$1]</span>');
+        });
+    
         Swal.fire({
             title: currentDay(),
-            html: '<div class="text-left">' + subjectsList.join("<br>") + '</div>',
+            html: '<div class="text-left">' + formattedSubjects.join("<br>") + '</div>',
             icon: 'info',
             footer: 'Пары начнутся в ' + arrivingTime,
             confirmButtonText: 'Ok',
-          })
+        });
     }
     else{
         Swal.fire({
